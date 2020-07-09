abd49a816b61cb58e933bf80277b4109bee5040d
DruceWillis <willis.druce@gmail.com>
2020-07-09 15:43:05 +0300

    Installed EntityFrameworkCore and EntityFrameworkSQLite

---
+++ Domain/Value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        
+        
+    }
+}
